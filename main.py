4d2a8fbd07f0016a3b515239fdd493bc2ee5f14a
pdmuds4 <ryosuke3191@gmail.com>
2025-05-31 00:07:21 +0900

    #1 middlewareにてcorsを追加する

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import uvicorn
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", tags=["root"])
 async def route():
