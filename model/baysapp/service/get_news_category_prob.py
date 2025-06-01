53bab7d8255c09d0f8287a591f0492d9d2c93bf4
pdmuds4 <ryosuke3191@gmail.com>
2025-06-01 20:39:01 +0900

    #2 GetNewsCategoryProbServiceにServiceErrorを追記

--- model/baysapp/service/get_news_category_prob.py
+++ model/baysapp/service/get_news_category_prob.py
@@ -1,7 +1,7 @@
 from clients import SqliteClient
 from ..._abstruct.service import ServiceModel
 from ...baysapp.valueObject import Word, Score
-
+from ..._error.service import ServiceError
 
 class GetNewsCategoryProbService(ServiceModel):
     client: SqliteClient
@@ -16,22 +16,29 @@ class GetNewsCategoryProbService(ServiceModel):
         table_name = self.client.table_name
         word = self.request.value
 
-        self.cursor.execute(
-            f"""
-                SELECT
-                    CAST((SELECT weather FROM {table_name} WHERE word='{word}') AS REAL) /
-                    CAST(SUM(weather) AS REAL),
-                    CAST((SELECT life FROM {table_name} WHERE word='{word}') AS REAL) /
-                    CAST(SUM(life) AS REAL),
-                    CAST((SELECT sports FROM {table_name} WHERE word='{word}') AS REAL) /
-                    CAST(SUM(sports) AS REAL),
-                    CAST((SELECT culture FROM {table_name} WHERE word='{word}') AS REAL) /
-                    CAST(SUM(culture) AS REAL),
-                    CAST((SELECT economy FROM {table_name} WHERE word='{word}') AS REAL) /
-                    CAST(SUM(economy) AS REAL)
-                FROM {table_name};
-            """
-        )
+        try:
+            self.cursor.execute(
+                f"""
+                    SELECT
+                        CAST((SELECT weather FROM {table_name} WHERE word='{word}') AS REAL) /
+                        CAST(SUM(weather) AS REAL),
+                        CAST((SELECT life FROM {table_name} WHERE word='{word}') AS REAL) /
+                        CAST(SUM(life) AS REAL),
+                        CAST((SELECT sports FROM {table_name} WHERE word='{word}') AS REAL) /
+                        CAST(SUM(sports) AS REAL),
+                        CAST((SELECT culture FROM {table_name} WHERE word='{word}') AS REAL) /
+                        CAST(SUM(culture) AS REAL),
+                        CAST((SELECT economy FROM {table_name} WHERE word='{word}') AS REAL) /
+                        CAST(SUM(economy) AS REAL)
+                    FROM {table_name};
+                """
+            )
+        except Exception as e:
+            raise ServiceError(
+                message = "SQLiteのクエリ実行中にエラーが発生しました。",
+                detail=str(e),
+                status_code=500
+            )
 
         result = self.cursor.fetchone()
 
