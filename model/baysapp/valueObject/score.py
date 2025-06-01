1d33b53f4e00984859ab2366c9a3ba27572fa13b
pdmuds4 <ryosuke3191@gmail.com>
2025-06-01 16:59:53 +0900

    #2 Scoreのvalueobjectを実装

---
+++ model/baysapp/valueObject/score.py
@@ -0,0 +1,14 @@
+from pydantic import field_validator
+
+from ..._abstruct import ValueObjectModel
+from ..._error import ValueObjectError
+
+class Score(ValueObjectModel):
+    value: float
+
+    @field_validator("value")
+    def check_value(cls, v):
+        if v < 0: raise ValueObjectError("0以下のスコアが検出されました。0以上1以下の値を指定してください。")
+        if v > 1: raise ValueObjectError("1以上のスコアが検出されました。0以上1以下の値を指定してください。")
+        
+        return v
