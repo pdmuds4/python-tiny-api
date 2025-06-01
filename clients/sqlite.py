9b020707558807e8aa8fb7c869140525a5b1d1c4
pdmuds4 <ryosuke3191@gmail.com>
2025-06-01 19:07:22 +0900

    #2 sqliteClientにtable_nameプロパティを追加

--- clients/sqlite.py
+++ clients/sqlite.py
@@ -1,5 +1,7 @@
 import sqlite3
 
 class SqliteClient(sqlite3.Connection):
-    def __init__(self, db_path: str):
-        super().__init__(db_path)
+    def __init__(self, db_path: str, table_name: str):
+        super().__init__(db_path)        
+        self.table_name = table_name
+
