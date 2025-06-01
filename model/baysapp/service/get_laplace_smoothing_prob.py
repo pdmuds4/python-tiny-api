d232e81670ed07c8d5676cc28887748587e606bb
pdmuds4 <ryosuke3191@gmail.com>
2025-06-01 20:39:34 +0900

    #2 GetLaplaceSmoothingProbServiceにServiceErrorを追記

--- model/baysapp/service/get_laplace_smoothing_prob.py
+++ model/baysapp/service/get_laplace_smoothing_prob.py
@@ -1,7 +1,7 @@
 from clients import SqliteClient
 from ..._abstruct.service import ServiceModel
 from ...baysapp.valueObject import Score
-
+from ..._error.service import ServiceError
 
 class GetLaplaceSmoothingProbService(ServiceModel):
     client: SqliteClient
@@ -14,32 +14,39 @@ class GetLaplaceSmoothingProbService(ServiceModel):
     def execute(self) -> list[Score] | None:
         table_name = self.client.table_name
 
-        self.cursor.execute(
-            f"""
-                SELECT
-                    1 / CAST(
-                        SUM(weather) + 
-                        (SELECT COUNT(*) FROM {table_name} WHERE weather != 0) AS REAL
-                    ),
-                    1 / CAST(
-                        SUM(life) + 
-                        (SELECT COUNT(*) FROM {table_name} WHERE life != 0) AS REAL
-                    ),
-                    1 / CAST(
-                        SUM(sports) + 
-                        (SELECT COUNT(*) FROM {table_name} WHERE sports != 0) AS REAL
-                    ),
-                    1 / CAST(
-                        SUM(culture) + 
-                        (SELECT COUNT(*) FROM {table_name} WHERE culture != 0) AS REAL
-                    ),
-                    1 / CAST(
-                        SUM(economy) + 
-                        (SELECT COUNT(*) FROM {table_name} WHERE economy != 0) AS REAL
-                    )
-                FROM {table_name};
-            """
-        )
+        try:
+            self.cursor.execute(
+                f"""
+                    SELECT
+                        1 / CAST(
+                            SUM(weather) + 
+                            (SELECT COUNT(*) FROM {table_name} WHERE weather != 0) AS REAL
+                        ),
+                        1 / CAST(
+                            SUM(life) + 
+                            (SELECT COUNT(*) FROM {table_name} WHERE life != 0) AS REAL
+                        ),
+                        1 / CAST(
+                            SUM(sports) + 
+                            (SELECT COUNT(*) FROM {table_name} WHERE sports != 0) AS REAL
+                        ),
+                        1 / CAST(
+                            SUM(culture) + 
+                            (SELECT COUNT(*) FROM {table_name} WHERE culture != 0) AS REAL
+                        ),
+                        1 / CAST(
+                            SUM(economy) + 
+                            (SELECT COUNT(*) FROM {table_name} WHERE economy != 0) AS REAL
+                        )
+                    FROM {table_name};
+                """
+            )
+        except Exception as e:
+            raise ServiceError(
+                message = "SQLiteのクエリ実行中にエラーが発生しました。",
+                detail=str(e),
+                status_code=500
+            )
 
         result = self.cursor.fetchone()
 
