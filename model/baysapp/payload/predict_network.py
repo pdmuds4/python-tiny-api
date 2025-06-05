e93e864cff0fee8a17a4a15bad360a41a143acf9
pdmuds4 <ryosuke3191@gmail.com>
2025-06-05 12:14:53 +0900

    #2 /baysapp/predict_networkを実装

--- model/baysapp/payload/predict_network.py
+++ model/baysapp/payload/predict_network.py
@@ -2,9 +2,16 @@ from pydantic import BaseModel
 from typing import Literal, Optional
 
 
+class EvidencePayload(BaseModel):
+    category: Optional[int]
+    sex: Optional[int]
+    time: Optional[int]
+    use_time: Optional[int]
+
+
 class PredictNetworkRequestPayload(BaseModel):
     type: Literal["category", "sex", "time", "use_time"]
-    evidence: dict[str, Optional[str]]
+    evidence: EvidencePayload
 
 
 class PredictNetworkResponsePayload(BaseModel):
--- routes/baysapp.py
+++ routes/baysapp.py
@@ -4,7 +4,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
-import MeCab
+import MeCab, pandas as pd
+from pgmpy.models import DiscreteBayesianNetwork
+from pgmpy.inference import VariableElimination
+
 from clients import SqliteClient
 from model._error import BaseError
 
@@ -20,6 +23,10 @@ router = APIRouter()
 
 sqlite_client = SqliteClient("./static_data/words.db", "words")
 work_repository = WordsRepository(client=sqlite_client)
+
+network_model = DiscreteBayesianNetwork([('sex', 'use_time'), ('time', 'use_time'), ('use_time', 'category')])
+network_model.fit(pd.read_csv('./static_data/screentime.csv'))
+network_infer = VariableElimination(network_model)
 
 
 @router.get("/baysapp", tags=["baysapp"])
@@ -52,6 +59,56 @@ async def naive(req: PredictNaiveRequestPayload):
             culture=response.culture.value,
             economy=response.economy.value
         )
+    except Exception as e:
+        if isinstance(e, BaseError):
+            return JSONResponse(
+                status_code=e.status_code,
+                content={
+                    "message": e.message,
+                    "detail": e.detail,
+                    "level": e.level
+                }
+            )
+        else:
+            return JSONResponse(
+                status_code=500,
+                content={
+                    "message": "An unexpected error occurred.",
+                    "detail": str(e),
+                    "level": "unknown"
+                }
+            )
+
+
+@router.post("/baysapp/predict_network", tags=["baysapp"], response_model=PredictNetworkResponsePayload)
+async def network(req: PredictNetworkRequestPayload):
+    try:
+        request = PredictNetworkRequestDTO(
+            type=PredictTypes(value=req.type),
+            evidence=EvidenceEntity(
+                category=CategoryEvidence(value=req.evidence.category),
+                sex=SexEvidence(value=req.evidence.sex),
+                time=TimeEvidence(value=req.evidence.time),
+                use_time=UseTimeEvidence(value=req.evidence.use_time)
+            )
+        )
+        
+        predictNetworkService = PredictNetworkService(client=network_infer)
+
+        predictNetworkUseCase = PredictNetworkUseCase(
+            request=request,
+            predictNetworkService=predictNetworkService
+        )
+
+        response = predictNetworkUseCase.execute()
+
+        return PredictNetworkResponsePayload(
+            type=response.type.value,
+            score={
+                category: score["value"] for category, score in response.score.model_dump().items()
+            }
+        )
+
     except Exception as e:
         if isinstance(e, BaseError):
             return JSONResponse(
