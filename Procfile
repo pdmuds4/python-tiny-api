324c67a7ede245f61f237d1d4d34476e1d2b6836
pdmuds4 <ryosuke3191@gmail.com>
2025-06-05 18:35:52 +0900

    #1 middlewareにapi key authを実装

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: uvicorn application:app --host 0.0.0.0 --port 8000
--- application.py
+++ application.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
+import os, dotenv, uvicorn
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
-import uvicorn
 
 from routes import *
 
 app = FastAPI()
+dotenv.load_dotenv()
+middleware_ignore_paths = ["/", "/docs", "/openapi.json"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,13 +17,35 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.middleware("http")
+async def api_key_authentication(request: Request, call_next):
+    api_key = os.getenv("API_KEY")
+    api_key_auth = os.getenv("API_KEY_AUTH")
+    
+    match api_key_auth:
+        case "true":
+            if request.url.path in middleware_ignore_paths or request.headers.get("X-API-KEY") == api_key:
+                return await call_next(request)
+            else:
+                return JSONResponse(
+                    status_code=401,
+                    content={"message": "Unauthorized: Invalid API Key"}
+                )
+        case "false":
+            return await call_next(request)
+
+
+
 @app.get("/", tags=["root"])
 async def route():
-    return JSONResponse({ "message": 'This is python-tiny-api!' })
+    return JSONResponse(
+        status_code=200,
+        content={ "message": 'This is python-tiny-api!' }
+    )
 
 
 app.include_router(baysAppRouter)
 
 
-# if __name__=="__main__":
-#     uvicorn.run("application:app", host="0.0.0.0", port=8000, reload=True)
+if __name__=="__main__":
+    uvicorn.run("application:app", host="0.0.0.0", port=8000, reload=True)
