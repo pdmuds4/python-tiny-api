3b76f1bc0a4cb846867dfd590c250e7057391922
pdmuds4 <ryosuke3191@gmail.com>
2025-06-20 16:51:52 +0900

    #5 middlewareのserverconfigを移動

--- routes/mhjoinup.py
+++ routes/mhjoinup.py
@@ -28,57 +28,36 @@ async def mhjoinup():
 
 @router.post("/mhjoinup/emit_webhook", tags=["mh-joinup"], response_model=EmitWebhookResponsePayload)
 async def emit_webhook(request: EmitWebhookRequestPayload):
-    try:
-        request_dto = EmitWebhookRequestDTO(
-            videoId=YoutubeId(value=request.videoId),
-            chatMessages=[
-                ChatMessageEntity(
-                    displayMessage=Message(value=chat_message.displayMessage),
-                    displayName=UserName(value=chat_message.displayName),
-                    photoUrl=UserAvatar(value=chat_message.photoUrl),
-                    platformAudienceId=ChannelId(value=chat_message.platformAudienceId)
-                ) for chat_message in request.chatMessages
-            ]
-        )
+    request_dto = EmitWebhookRequestDTO(
+        videoId=YoutubeId(value=request.videoId),
+        chatMessages=[
+            ChatMessageEntity(
+                displayMessage=Message(value=chat_message.displayMessage),
+                displayName=UserName(value=chat_message.displayName),
+                photoUrl=UserAvatar(value=chat_message.photoUrl),
+                platformAudienceId=ChannelId(value=chat_message.platformAudienceId)
+            ) for chat_message in request.chatMessages
+        ]
+    )
 
-        emitWebhookUseCase = EmitWebhookUseCase(request=request_dto)
-        response = emitWebhookUseCase.execute()
+    emitWebhookUseCase = EmitWebhookUseCase(request=request_dto)
+    response = emitWebhookUseCase.execute()
 
-        response_payload = EmitWebhookResponsePayload(
-            youtube_id=response.youtube_id.value,
-            chat_data=[
-                ChatDataEntityTypes(
-                    message=chat_data.message.value,
-                    name=chat_data.name.value,
-                    avatar=chat_data.avatar.value,
-                    channel_id=chat_data.channel_id.value
-                ) for chat_data in response.chat_data
-            ]
-        )
+    response_payload = EmitWebhookResponsePayload(
+        youtube_id=response.youtube_id.value,
+        chat_data=[
+            ChatDataEntityTypes(
+                message=chat_data.message.value,
+                name=chat_data.name.value,
+                avatar=chat_data.avatar.value,
+                channel_id=chat_data.channel_id.value
+            ) for chat_data in response.chat_data
+        ]
+    )
 
-        await sio.emit(request.videoId, response_payload.model_dump())
+    await sio.emit(request.videoId, response_payload.model_dump())
 
-        return response_payload
-    except Exception as e:
-        if isinstance(e, BaseError):
-            return JSONResponse(
-                status_code=e.status_code,
-                content={
-                    "message": e.message,
-                    "detail": e.detail,
-                    "level": e.level
-                }
-            )
-        else:
-            return JSONResponse(
-                status_code=500,
-                content={
-                    "message": "An unexpected error occurred.",
-                    "detail": str(e),
-                    "level": "unknown"
-                }
-            )
-
+    return response_payload
 
 
 @sio.on('connect')
